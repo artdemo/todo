ef31a0d1cc66a580f77d72bcb16fb56fa6e1d29e
Artem <nickmail@inbox.ru>
2021-02-22 15:25:40 +0400

    Add api methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from './Form';
 import List from './List';
 import Task from './Task';
-
-const mock = [
-  { text: 'Task 1', isChecked: true, id: 1 },
-  { text: 'Task 2', isChecked: false, id: 2 },
-];
+import axios from './api.js';
 
 const renderTasks = (tasks) =>
   tasks.map(({ id, text, isChecked }) => (
@@ -16,7 +12,50 @@ const renderTasks = (tasks) =>
   ));
 
 const App = () => {
-  const [tasks, setTasks] = useState(mock);
+  const [tasks, setTasks] = useState([]);
+
+  const readData = () =>
+    axios
+      .get()
+      .then((response) => {
+        setTasks(response.data);
+        return response;
+      })
+      .catch((error) => {
+        console.dir(error);
+        return error;
+      });
+
+  const createData = (data) =>
+    axios
+      .post('', data)
+      .then(() => readData())
+      .catch((error) => {
+        console.dir(error);
+        return error;
+      });
+
+  const updateData = (id, data) =>
+    axios
+      .put(`${id}`, data)
+      .then(() => readData())
+      .catch((error) => {
+        console.dir(error);
+        return error;
+      });
+
+  const deleteData = (id) =>
+    axios
+      .put(`${id}`)
+      .then(() => readData())
+      .catch((error) => {
+        console.dir(error);
+        return error;
+      });
+
+  useEffect(() => {
+    readData();
+  }, []);
 
   return (
     <div>
