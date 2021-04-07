2c115f76b2b6ce1192e8f52a922c69faf94a4599
Artem <nickmail@inbox.ru>
2021-04-07 21:41:15 +0400

    Fix api exports

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,31 +1,29 @@
 import axios from 'axios';
 
-const instance = axios.create({
+export const instance = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/todos`,
 });
 
-const getAllTasks = () =>
+export const getAllTasks = () =>
   instance.get().catch((e) => {
     console.dir(e);
     throw e;
   });
 
-const postNewTask = (data) =>
+export const postNewTask = (data) =>
   instance.post('', data).catch((e) => {
     console.dir(e);
     throw e;
   });
 
-const putTask = (id, data) =>
+export const putTask = (id, data) =>
   instance.put(`${id}`, data).catch((e) => {
     console.dir(e);
     throw e;
   });
 
-const deleteTask = (id) =>
+export const deleteTask = (id) =>
   instance.delete(`${id}`).catch((e) => {
     console.dir(e);
     throw e;
   });
-
-export { getAllTasks, postNewTask, putTask, deleteTask };
